878ff30688f3f48c6455d7baf4ae8345cb739ca2
Vladimir Pechev <vladimir.pechev@scalefocus.com>
2016-05-03 23:57:29 +0300

    create sample project structure

---
+++ resources/php/navigation.php
@@ -0,0 +1,9 @@
+<nav>
+    <div id="main-navigation" class="navigation">
+        <ul class="nav">
+            <li> <span id="home-link">Начало</span> </li>
+            <li> <span>Секция 1</span> </li>
+            <li> <span id="contacts-link">Контакти</span></li>
+        </ul>
+    </div>
+</nav>
